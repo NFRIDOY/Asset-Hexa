bbed8e7c80e0c4bb98bdd3679a351f633a0bc918
mehedi505991 <mdmehediislammoksedul@gmail.com>
2024-01-16 22:03:49 +0600

    complite about section

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,193 @@
+import aboutImg from '../../assets/AboutPic.png'
+import avaterImg from '../../assets/avater.png'
+import { FaPhoneAlt } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+
+const About = () => {
+    return (
+        <div className=' max-w-[1450px] mx-auto'>
+            <div className="bg-[#262626] h-72 pt-10">
+                <h1 className="ml-20 mb-2 text-6xl font-bold text-white">ABOUT US</h1>
+                <h3 className="ml-20 mb-1 text-[#DDD831] text-xl " >GREAT COMPANY</h3>
+                <p className="ml-20 text-[#9BA39e] w-[800px] ">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.</p>
+            </div>
+            <div className=" grid grid-cols-1 md:grid-cols-2 gap-5 mt-10">
+                <div>
+                    <img src={aboutImg} alt="" />
+                </div>
+                <div>
+                    <p className="text-[#E2D831]">Company Value</p>
+                    <p className="text-[#33373E] text-4xl mb-2">Who Are You</p>
+                    <p className="text-[#777777]">Coop Bank is a company of the envato Foundation through its banking activities to contribute in overcoming the structural causes of poverty in Australia.</p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-2">
+                        <div>
+                            <p className="text-[#333776] mb-1">Our Mission</p>
+                            <p className="text-[#A7A3AB]">Lorem iur adipiscing elit. Ut vehicula dapibus augue nec maximustiam eleifend magna erat, quis vestibulum lacus mattis sit ametec pellentesque lorem sapien.</p>
+                        </div>
+                        <div>
+                            <p className="text-[#333776] mb-1">Responsibilty</p>
+                            <p className="text-[#A7A3AB]">Lorem iur adipiscing elit. Ut vehicula dapibus augue nec maximustiam eleifend magna erat, quis vestibulum lacus mattis sit ametec pellentesque lorem sapien.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className='mt-10'>
+                <h2 className='text-3xl text-center mb-2'><span className='text-[#E1DD4A] '>Team </span>Members</h2>
+                <p className='text-2xl text-center mb-10'>We have created alliances with recognized entities that contribute to improving quality of your life.</p>
+                <div className='gap-5 grid grid-cols-1 md:grid-cols-3'>
+                    <div className=' border '>
+                        <p className='bg-[#DDD831] h-5 mb-5 '></p>
+                        <div className='flex justify-center'>
+                            <img className='mb-3 h-32 rounded-full ' src={avaterImg} alt="" />
+                        </div>
+                        <h2 className='text-2xl font-bold text-center mb-3'>Mehedi</h2>
+
+                        <div className='flex justify-center gap-2'>
+                            <FaPhoneAlt className='mt-1' style={20} />
+                            <p>ggg@gmail.com</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiMail className='mt-1' style={20} />
+                            <p>+56 333333333</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <FaFacebook className='mt-1' style={20} />
+                            <p>facebook</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiLinkedin className='mt-1' style={20} />
+                            <p className='pb-10'>Linkdin</p>
+                        </div>
+                    </div>
+                    <div className=' border '>
+                        <p className='bg-[#DDD831] h-5 mb-5 '></p>
+                        <div className='flex justify-center'>
+                            <img className='mb-3 h-32 rounded-full ' src={avaterImg} alt="" />
+                        </div>
+                        <h2 className='text-2xl font-bold text-center mb-3'>Mehedi</h2>
+
+                        <div className='flex justify-center gap-2'>
+                            <FaPhoneAlt className='mt-1' style={20} />
+                            <p>ggg@gmail.com</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiMail className='mt-1' style={20} />
+                            <p>+56 333333333</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <FaFacebook className='mt-1' style={20} />
+                            <p>facebook</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiLinkedin className='mt-1' style={20} />
+                            <p>Linkdin</p>
+                        </div>
+                    </div>
+                    <div className=' border '>
+                        <p className='bg-[#DDD831] h-5 mb-5 '></p>
+                        <div className='flex justify-center'>
+                            <img className='mb-3 h-32 rounded-full ' src={avaterImg} alt="" />
+                        </div>
+                        <h2 className='text-2xl font-bold text-center mb-3'>Mehedi</h2>
+
+                        <div className='flex justify-center gap-2'>
+                            <FaPhoneAlt className='mt-1' style={20} />
+                            <p>ggg@gmail.com</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiMail className='mt-1' style={20} />
+                            <p>+56 333333333</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <FaFacebook className='mt-1' style={20} />
+                            <p>facebook</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiLinkedin className='mt-1' style={20} />
+                            <p>Linkdin</p>
+                        </div>
+                    </div>
+                    <div className=' border '>
+                        <p className='bg-[#DDD831] h-5 mb-5 '></p>
+                        <div className='flex justify-center'>
+                            <img className='mb-3 h-32 rounded-full ' src={avaterImg} alt="" />
+                        </div>
+                        <h2 className='text-2xl font-bold text-center mb-3'>Mehedi</h2>
+
+                        <div className='flex justify-center gap-2'>
+                            <FaPhoneAlt className='mt-1' style={20} />
+                            <p>ggg@gmail.com</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiMail className='mt-1' style={20} />
+                            <p>+56 333333333</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <FaFacebook className='mt-1' style={20} />
+                            <p>facebook</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiLinkedin className='mt-1' style={20} />
+                            <p>Linkdin</p>
+                        </div>
+                    </div>
+                    <div className=' border '>
+                        <p className='bg-[#DDD831] h-5 mb-5 '></p>
+                        <div className='flex justify-center'>
+                            <img className='mb-3 h-32 rounded-full ' src={avaterImg} alt="" />
+                        </div>
+                        <h2 className='text-2xl font-bold text-center mb-3'>Mehedi</h2>
+
+                        <div className='flex justify-center gap-2'>
+                            <FaPhoneAlt className='mt-1' style={20} />
+                            <p>ggg@gmail.com</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiMail className='mt-1' style={20} />
+                            <p>+56 333333333</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <FaFacebook className='mt-1' style={20} />
+                            <p>facebook</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiLinkedin className='mt-1' style={20} />
+                            <p>Linkdin</p>
+                        </div>
+                    </div>
+                    <div className=' border '>
+                        <p className='bg-[#DDD831] h-5 mb-5 '></p>
+                        <div className='flex justify-center'>
+                            <img className='mb-3 h-32 rounded-full ' src={avaterImg} alt="" />
+                        </div>
+                        <h2 className='text-2xl font-bold text-center mb-3'>Mehedi</h2>
+
+                        <div className='flex justify-center gap-2'>
+                            <FaPhoneAlt className='mt-1' style={20} />
+                            <p>ggg@gmail.com</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiMail className='mt-1' style={20} />
+                            <p>+56 333333333</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <FaFacebook className='mt-1' style={20} />
+                            <p>facebook</p>
+                        </div>
+                        <div className='flex justify-center gap-2'>
+                            <CiLinkedin className='mt-1' style={20} />
+                            <p className='pb-10'>Linkdin</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default About;
