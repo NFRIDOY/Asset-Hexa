8596f4b50b5ed309c8e453127bd9a7295608fb60
Mustafiz82e <mustafiz8260@gamil.com>
2024-01-18 19:51:06 +0600

    helpDesk Page completed

--- src/Components/HelpDesk/HelpDesk.jsx
+++ src/Components/HelpDesk/HelpDesk.jsx
@@ -1,10 +1,14 @@
 import Faq from "./Faq";
+import HowItWorks from "./HowItWorks";
+import WhyChooseUs from "./WhyChooseUs";
 
 
 const HelpDesk = () => {
     return (
         <div>
+            <HowItWorks></HowItWorks>
             <Faq></Faq>
+            <WhyChooseUs></WhyChooseUs>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor : "#23A455"
+      }
+    },
+    
   },
   plugins: [require("daisyui")],
 }
