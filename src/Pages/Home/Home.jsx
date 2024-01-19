3785ddee7a30de1e6cd127c98e291d7fb89ea75a
Mustafiz82e <mustafiz8260@gamil.com>
2024-01-19 11:05:43 +0600

    some experiment

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,18 @@
-
-
-
 import Banner from "./Banner";
-
+import { FaPhoneAlt } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import arif from "../../assets/logo/favicon.png";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            
-        </div>
-    );
+	return (
+		<div>
+			<Banner></Banner>
+			
+		</div>
+	);
 };
 
 export default Home;
