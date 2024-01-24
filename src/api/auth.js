db4386e6998fd2a8ad8414502900da11fd796de9
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-01-24 06:26:56 +0600

    update signup and login also implement imgbb and save user in database

---
+++ src/api/auth.js
@@ -0,0 +1,15 @@
+import axiosSecure from "."
+
+
+// Save user data in database
+export const saveUser = async user => {
+    const currentUser = {
+      email: user.email,
+      role: 'guest',
+      status: 'Verified',
+    }
+    const { data } = await axiosSecure.put(`/users/${user?.email}`, currentUser)
+  
+    
+    return data
+  }
