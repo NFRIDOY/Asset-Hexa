4309637c804ebada1d6169cd36d1325d435c09ad
Radiat09 <rzsianboyridoy10@gmail.com>
2024-01-24 19:45:30 +0600

    installed react tab and built basic structure

--- src/Dashboard/Transection.jsx
+++ src/Dashboard/Transection.jsx
@@ -1,11 +1,25 @@
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Transection = () => {
-    return (
-        <div>
-            <h2>Transection</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Transection</h2>
+      <Tabs>
+        <TabList className={"flex justify-center border-b"}>
+          <Tab>Expense</Tab>
+          <Tab>Income</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Transection;
