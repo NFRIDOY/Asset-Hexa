5b840a0aa288cfbe7cff124096608459728f4eb1
Radiat09 <rzsianboyridoy10@gmail.com>
2024-01-26 16:33:03 +0600

    coustomized pie chart -added coustom color to each pie part

--- src/Components/TransectionColumn.jsx
+++ src/Components/TransectionColumn.jsx
@@ -6,35 +6,37 @@ import useAuth from "../hooks/useAuth";
 import useAxios from "../hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
 
-const TransectionColumn = ({transactionType}) => {
-
-  const axiosPublic = useAxios()
-  const {user}=useAuth()
+const TransectionColumn = ({ transactionType }) => {
+  const axiosPublic = useAxios();
+  const { user } = useAuth();
 
   const { data: transection = [] } = useQuery({
-    queryKey: ['transection'],
+    queryKey: ["transection"],
     queryFn: async () => {
-        const res = await axiosPublic.get(`/transections?type=${transactionType}&email=${user?.email}`)
-        return res.data
+      const res = await axiosPublic.get(
+        `/transections?type=${transactionType}&email=${user?.email}`
+      );
+      return res.data;
+    },
+  });
+  console.log(transection);
 
-    }
-
-})
-
-
-  return (
-    transection.map(transectionItem=>(<div key={transectionItem._id} className="flex items-center justify-around border rounded-md p-4">
-    <h4 className="text-xl font-bold">{transectionItem.category}</h4>
-    <div className="felx felx-col">
-      <p className="font-medium">{transectionItem.note}</p>
-    </div>
-    <p className="text-lg font-bold text-blue-500">${transectionItem.amount}</p>
-    {/* <MdDeleteForever className="text-3xl text-red-600"></MdDeleteForever>
+  return transection.map((transectionItem) => (
+    <div
+      key={transectionItem._id}
+      className="flex items-center justify-around border rounded-md p-4"
+    >
+      <h4 className="text-xl font-bold">{transectionItem.category}</h4>
+      <div className="felx felx-col">
+        <p className="font-medium">{transectionItem.note}</p>
+      </div>
+      <p className="text-lg font-bold text-blue-500">
+        ${transectionItem.amount}
+      </p>
+      {/* <MdDeleteForever className="text-3xl text-red-600"></MdDeleteForever>
     <GrDocumentUpdate className="text-2xl text-green-600"></GrDocumentUpdate> */}
-  </div> ))
-  );
+    </div>
+  ));
 };
 
 export default TransectionColumn;
-
-
