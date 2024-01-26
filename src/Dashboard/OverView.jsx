29f311035c75cc6ee954fe1e702ac7c8b6ff93a7
Mustafiz82e <mustafiz8260@gamil.com>
2024-01-26 19:23:28 +0600

    linechart replaced with piechart

--- src/Dashboard/OverView.jsx
+++ src/Dashboard/OverView.jsx
@@ -1,116 +1,145 @@
-import { useContext, useState } from "react";
-import "../../src/App.css"
-import {
-	LineChart,
-	Line,
-	XAxis,
-	YAxis,
-	CartesianGrid,
-	Tooltip,
-	Legend,
-	ResponsiveContainer,
-} from "recharts";
+import { useContext, useEffect, useState } from "react";
+import "../../src/App.css";
+// import {
+// 	LineChart,
+// 	Line,
+// 	XAxis,
+// 	YAxis,
+// 	CartesianGrid,
+// 	Tooltip,
+// 	Legend,
+// 	ResponsiveContainer,
+// } from "recharts";
 import { AuthContext } from "../providers/AuthProvider";
 import useAxios from "../hooks/useAxios";
 import toast from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
-
+import { PieChart, Pie, Tooltip, Legend, Cell } from "recharts";
 
 const OverView = () => {
 	const [incomeText, setIncomeText] = useState("");
 	const [expanseText, setExpanseText] = useState("");
 	const [transferText, setTransferText] = useState("");
-    // const [transferData , setTransferData] = useState("") 
-    const axiosPublic = useAxios()
-    const {user}= useContext(AuthContext)
-    
-    // https://asset-hexa-server.vercel.app/transections?type=EXPENSE&email=backend@example.com
-    const { data: transferData = [] , refetch} = useQuery({
-        queryKey: ["transeferData"],
-        queryFn: async () => {
-          const res = await axiosPublic.get(
-            `/transections?type=TRANSFER&email=${user?.email}`
-          );
-          return res.data;
-        },
-    });
-    // console.log(transection);
-    //   console.log(transection);
+	// const [transferData , setTransferData] = useState("")
+	const axiosPublic = useAxios();
+	const { user } = useContext(AuthContext);
+	const [pieData, setPieData] = useState([]);
+	const [pieLabel, setPieLabel] = useState([]);
 
 
-	const data = [
-		{
-			name: "Jan",
-			Income: 4000,
-			Expenses: 2400,
-			amt: 2400,
+    useEffect(() => {
+        axiosPublic.get("/accountPi?email=front@example.com").then((res) => {
+          //   console.log(res.data);
+          setPieData(res.data.accPiData);
+          setPieLabel(res.data.accPiLebel);
+        });
+      }, [axiosPublic]);
+
+	// https://asset-hexa-server.vercel.app/transections?ty pe=EXPENSE&email=backend@example.com
+	const { data: transferData = [], refetch } = useQuery({
+		queryKey: ["transeferData"],
+		queryFn: async () => {
+			const res = await axiosPublic.get(
+				`/transections?type=TRANSFER&email=${user?.email}`
+			);
+			return res.data;
 		},
-		{
-			name: "Feb",
-			Income: 3000,
-			Expenses: 1398,
-			amt: 2210,
-		},
-		{
-			name: "March",
-			Income: 2000,
-			Expenses: 9800,
-			amt: 2290,
-		},
-		{
-			name: "April",
-			Income: 2780,
-			Expenses: 3908,
-			amt: 2000,
-		},
-		{
-			name: "May",
-			Income: 1890,
-			Expenses: 4800,
-			amt: 2181,
-		},
-		{
-			name: "June",
-			Income: 2390,
-			Expenses: 3800,
-			amt: 2500,
-		},
-		{
-			name: "July",
-			Income: 3490,
-			Expenses: 4300,
-			amt: 2100,
-		},
-		{
-			name: "Aug",
-			Income: 3490,
-			Expenses: 4300,
-			amt: 2100,
-		},
-		{
-			name: "Sep",
-			Income: 3490,
-			Expenses: 4300,
-			amt: 2100,
-		},
-		{
-			name: "Oct",
-			Income: 3490,
-			Expenses: 4300,
-			amt: 2100,
-		},
-		{
-			name: "Nov",
-			Income: 3490,
-			Expenses: 4300,
-			amt: 2100,
-		},
-		{
-			name: "Dec",
-			Income: 3490,
-			Expenses: 4300,
-			amt: 2100,
-		},
+	});
+	// console.log(transection);
+	  console.log(transferData);
+
+	// const data = [
+	// 	{
+	// 		name: "Jan",
+	// 		Income: 4000,
+	// 		Expenses: 2400,
+	// 		amt: 2400,
+	// 	},
+	// 	{
+	// 		name: "Feb",
+	// 		Income: 3000,
+	// 		Expenses: 1398,
+	// 		amt: 2210,
+	// 	},
+	// 	{
+	// 		name: "March",
+	// 		Income: 2000,
+	// 		Expenses: 9800,
+	// 		amt: 2290,
+	// 	},
+	// 	{
+	// 		name: "April",
+	// 		Income: 2780,
+	// 		Expenses: 3908,
+	// 		amt: 2000,
+	// 	},
+	// 	{
+	// 		name: "May",
+	// 		Income: 1890,
+	// 		Expenses: 4800,
+	// 		amt: 2181,
+	// 	},
+	// 	{
+	// 		name: "June",
+	// 		Income: 2390,
+	// 		Expenses: 3800,
+	// 		amt: 2500,
+	// 	},
+	// 	{
+	// 		name: "July",
+	// 		Income: 3490,
+	// 		Expenses: 4300,
+	// 		amt: 2100,
+	// 	},
+	// 	{
+	// 		name: "Aug",
+	// 		Income: 3490,
+	// 		Expenses: 4300,
+	// 		amt: 2100,
+	// 	},
+	// 	{
+	// 		name: "Sep",
+	// 		Income: 3490,
+	// 		Expenses: 4300,
+	// 		amt: 2100,
+	// 	},
+	// 	{
+	// 		name: "Oct",
+	// 		Income: 3490,
+	// 		Expenses: 4300,
+	// 		amt: 2100,
+	// 	},
+	// 	{
+	// 		name: "Nov",
+	// 		Income: 3490,
+	// 		Expenses: 4300,
+	// 		amt: 2100,
+	// 	},
+	// 	{
+	// 		name: "Dec",
+	// 		Income: 3490,
+	// 		Expenses: 4300,
+	// 		amt: 2100,
+	// 	},
+	// ];
+
+	const data01 = [
+		{ name: pieLabel[0], value: pieData[0] },
+		{ name: pieLabel[1], value: pieData[1] },
+		{ name: pieLabel[2], value: 500 },
+		{ name: pieLabel[3], value: 400 },
+		{ name: pieLabel[4], value: 100 },
+		{ name: pieLabel[5], value: pieData[5] },
+		{ name: pieLabel[6], value: pieData[5] },
+	];
+
+	const COLORS = [
+		"#0088FE",
+		"#00C49F",
+		"#FFBB28",
+		"#FF8042",
+		"#ff006e",
+		"#fb6f92",
 	];
 
 	const handleSubmitIncome = (e) => {
@@ -121,8 +150,8 @@ const OverView = () => {
 		const category = form.category.value;
 		const account = form.account.value;
 		const note = form.note.value;
-        const email = user.email
-        const type = "INCOME"
+		const email = user.email;
+		const type = "INCOME";
 
 		if (
 			date == "Invalid Date" ||
@@ -133,20 +162,24 @@ const OverView = () => {
 		) {
 			setIncomeText("please fill out all the form");
 		} else {
-			const incomeData = {email, date, amount, category, account, note , type  };
+			const incomeData = {
+				email,
+				date,
+				amount,
+				category,
+				account,
+				note,
+				type,
+			};
 			setIncomeText("");
-			console.log(incomeData) , "thiw is incoem t=data";
+			console.log(incomeData), "thiw is incoem t=data";
 			form.reset();
-            axiosPublic.post("/transections" , incomeData)
-            .then(res => {
-                console.log(res.data);
-                if(res?.data.resultAccount.acknowledged){
-                    toast.success('Income Data added Successfully')
-
-                }
-            })
-            
-
+			axiosPublic.post("/transections", incomeData).then((res) => {
+				console.log(res.data);
+				if (res?.data.resultAccount.acknowledged) {
+					toast.success("Income Data added Successfully");
+				}
+			});
 		}
 	};
 
@@ -158,8 +191,8 @@ const OverView = () => {
 		const category = form.category.value;
 		const account = form.account.value;
 		const note = form.note.value;
-        const email = user.email
-        const type = "EXPENSE"
+		const email = user.email;
+		const type = "EXPENSE";
 
 		if (
 			date == "Invalid Date" ||
@@ -170,33 +203,37 @@ const OverView = () => {
 		) {
 			setExpanseText("please fill out all the form");
 		} else {
-			const expanseData = {email, date, amount, category, account, note , type };
+			const expanseData = {
+				email,
+				date,
+				amount,
+				category,
+				account,
+				note,
+				type,
+			};
 			setExpanseText("");
 			console.log(expanseData);
-            form.reset();
-            axiosPublic.post("/transections" , expanseData)
-            .then(res => {
-                console.log(res.data);
-                if(res?.data.resultAccount.acknowledged){
-                    toast.success('Expanse Data added Successfully')
-
-                }
-            })
-
-
+			form.reset();
+			axiosPublic.post("/transections", expanseData).then((res) => {
+				console.log(res.data);
+				if (res?.data.resultAccount.acknowledged) {
+					toast.success("Expanse Data added Successfully");
+				}
+			});
 		}
 	};
 
 	const handleSubmittransfer = (e) => {
 		e.preventDefault();
 		const form = e.target;
-		const date = form.date.value
+		const date = form.date.value;
 		const amount = form.amount.value;
 		const from = form.from.value;
 		const to = form.to.value;
 		const note = form.note.value;
-        const email = user.email
-        const type = "TRANSFER"
+		const email = user.email;
+		const type = "TRANSFER";
 
 		if (
 			date == "Invalid Date" ||
@@ -207,31 +244,23 @@ const OverView = () => {
 		) {
 			setTransferText("please fill out all the form");
 		} else {
-			const transferData = {email , date, amount, from, to, note ,type  };
+			const transferData = { email, date, amount, from, to, note, type };
 			setTransferText("");
 			console.log(transferData);
 			form.reset();
-            axiosPublic.post("/transections" , transferData)
-            .then(res => {
-                console.log(res.data);
-                if(res?.data.resultAccount.acknowledged){
-                    toast.success('Transfer Data added Successfully')
-
-                }
-            })
+			axiosPublic.post("/transections", transferData).then((res) => {
+				console.log(res.data);
+				if (res?.data.resultAccount.acknowledged) {
+					toast.success("Transfer Data added Successfully");
+				}
+			});
 		}
 	};
 
-
-
-    
-
-
-
 	return (
-		<div>
-			<div className="flex flex-col lg:flex-row gap-5">
-				<div className="lg:overflow-x-auto">
+		<div className=" ">
+			<div className="flex justify-around flex-col items-center lg:flex-row gap-5">
+				{/* <div className="lg:overflow-x-auto">
 					<LineChart
 						width={700}
 						height={400}
@@ -260,29 +289,63 @@ const OverView = () => {
 							stroke="#82ca9d"
 						/>
 					</LineChart>
+				</div> */}
+
+				<div className="flex justify-around flex-col items-center overflow-clip">
+					
+					<PieChart width={400} height={400}>
+						<Pie
+							dataKey="value"
+							isAnimationActive={false}
+							data={data01}
+							cx="50%"
+							cy="50%"
+							outerRadius={80}
+							fill="#8884d8"
+							label
+						>
+							{data01?.map((entry, index) => (
+								<Cell
+									key={`cell-${index}`}
+									fill={COLORS[index % COLORS.length]}
+								/>
+							))}
+						</Pie>
+
+						<Tooltip />
+						<Legend />
+					</PieChart>
 				</div>
 				<div>
 					<h1 className="text-xl font-medium text-center mb-5">
 						Transections{" "}
 					</h1>
 					<div className="overflow-x-auto">
-						<table className="table">
+						<table className="table text-center">
 							{/* head */}
 							<thead>
 								<tr>
+									<th>Date</th>
+									<th>Time</th>
 									<th>from</th>
 									<th>To</th>
 									<th>amount</th>
 								</tr>
 							</thead>
 							<tbody>
-                            {transferData?.map((item) => <tr key={item?.id} className="hover">
-									<td> {item?.from} </td>
-									<td>{item?.to}</td>
-									<td>{item?.amount}</td>
-								</tr> )}
-								
-								
+								{transferData?.map((item) => (
+									<tr key={item?.id} className="hover">
+										<td> {new Date(
+									item?.date
+								).toLocaleDateString()} </td>
+										<td> {new Date(
+									item?.date
+								).toLocaleTimeString()} </td>
+										<td> {item?.from} </td>
+										<td>{item?.to}</td>
+										<td>${item?.amount}</td>
+									</tr>
+								))}
 							</tbody>
 						</table>
 					</div>
@@ -329,23 +392,20 @@ const OverView = () => {
 							</g>{" "}
 						</svg>{" "}
 					</span>{" "}
-					<span className="-z-10 bg-gradient-to-tr bottom-0 left-1/2  transform -translate-x-1/2  from-[#0d87f8]/80 to-[#70c4ff]/80 duration-300  absolute   rounded-full  z-20 w-0 h-0   group-hover:w-[130px] group-hover:h-[130px]">
-                   </span>{" "}
-					<span className=" bg-gradient-to-tr bottom-0 left-1/2 from-[#0d87f8]/50 to-[#70c4ff]/50 transform -translate-x-1/2 duration-500  absolute  rounded-full  z-20 w-0 h-0  group-hover:w-[200px] group-hover:h-[200px] hover:duration-300 group-hover:block ">
-                    </span>{" "}
-					<span className=" bg-gradient-to-tr bottom-0 left-1/2 from-[#0d87f8]/50 to-[#70c4ff]/50 transform -translate-x-1/2 duration-500  absolute  rounded-full  z-20 w-0 h-0  group-hover:w-[260px] group-hover:h-[260px] hover:duration-300 group-hover:block ">
-                    </span>{" "}
+					<span className="-z-10 bg-gradient-to-tr bottom-0 left-1/2  transform -translate-x-1/2  from-[#0d87f8]/80 to-[#70c4ff]/80 duration-300  absolute   rounded-full  z-20 w-0 h-0   group-hover:w-[130px] group-hover:h-[130px]"></span>{" "}
+					<span className=" bg-gradient-to-tr bottom-0 left-1/2 from-[#0d87f8]/50 to-[#70c4ff]/50 transform -translate-x-1/2 duration-500  absolute  rounded-full  z-20 w-0 h-0  group-hover:w-[200px] group-hover:h-[200px] hover:duration-300 group-hover:block "></span>{" "}
+					<span className=" bg-gradient-to-tr bottom-0 left-1/2 from-[#0d87f8]/50 to-[#70c4ff]/50 transform -translate-x-1/2 duration-500  absolute  rounded-full  z-20 w-0 h-0  group-hover:w-[260px] group-hover:h-[260px] hover:duration-300 group-hover:block "></span>{" "}
 				</button>
 
 				<button
 					onClick={() => {
-                        document.getElementById("modal_income").showModal();
-                    }}
+						document.getElementById("modal_income").showModal();
+					}}
 					className="custom-button income"
 				>
 					Add Income
 				</button>
-                <button
+				<button
 					onClick={() =>
 						document.getElementById("modal_expanse").showModal()
 					}
@@ -353,7 +413,7 @@ const OverView = () => {
 				>
 					Add Expence{" "}
 				</button>
-				
+
 				<button
 					onClick={() =>
 						document.getElementById("modal_transfer").showModal()
@@ -517,9 +577,8 @@ const OverView = () => {
 							name="amount"
 							type="number"
 							placeholder="Amount"
-                            min="0"
-                            oninput="validity.valid||(value='');"
-
+							min="0"
+							oninput="validity.valid||(value='');"
 							className="input input-bordered w-full "
 						/>
 						<select
