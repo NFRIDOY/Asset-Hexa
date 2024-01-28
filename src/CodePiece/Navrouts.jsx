2abe13bf95df379d126f3e7abc3da84426f194c2
Mustafiz82e <mustafiz8260@gamil.com>
2024-01-29 00:06:24 +0600

    nav updated

--- src/CodePiece/Navrouts.jsx
+++ src/CodePiece/Navrouts.jsx
@@ -1,109 +1,109 @@
-import React from 'react';
+import React from "react";
 import logo from "../assets/logo/logo.png";
 import { Link, NavLink } from "react-router-dom";
 
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import NavUl from './NavUl';
+import NavUl from "./NavUl";
 
 const Navrouts = () => {
-    
-    const { user, logOut } = useContext(AuthContext);
-   
+	const { user, logOut } = useContext(AuthContext);
 
+	return (
+		<div className="w-full navbar bg-base-300">
+			<div className="flex-none lg:hidden">
+				<label
+					htmlFor="my-drawer-3"
+					aria-label="open sidebar"
+					className="btn btn-square btn-ghost"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						className="inline-block w-6 h-6 stroke-current"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path>
+					</svg>
+				</label>
+			</div>
 
-    return (
-       <div className="w-full navbar bg-base-300">
-							<div className="flex-none lg:hidden">
-								<label
-									htmlFor="my-drawer-3"
-									aria-label="open sidebar"
-									className="btn btn-square btn-ghost"
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										className="inline-block w-6 h-6 stroke-current"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M4 6h16M4 12h16M4 18h16"
-										></path>
-									</svg>
-								</label>
-							</div>
+			<div className="navbar p-0 bg-base-100">
+				<div className="navbar-start">
+					<Link to="/" className="btn btn-ghost text-xl">
+						<img className="h-14" src={logo} alt="" />
+					</Link>
+				</div>
+				<div className="navbar-center hidden lg:flex">
+					<ul className="menu menu-horizontal gap-4 px-1">
+						<NavUl></NavUl>
+					</ul>
+				</div>
+				{/* Login Logout toggle button logic */}
+				<div className="navbar-end mr-2">
+					{user?.email ? (
+						// <div className="flex gap-3 items-center">
+						// 	<div className="dropdown dropdown-end">
+						// 		<div
+						// 			tabIndex={0}
+						// 			role="button"
+						// 			className="btn btn-ghost btn-circle avatar"
+						// 		>
+						// 			<div id="my-drawer-2" className="w-10 rounded-full">
+						// 				{
+						//                     user?.photoURL ? <img src={user.photoURL} alt="" /> : <img
+						// 					alt="Tailwind CSS Navbar component"
+						// 					src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+						// 				/>
+						//                 }
+						// 			</div>
+						// 		</div>
+						// 		<ul style={{backdropFilter: "blur(30px)"}}
+						// 			tabIndex={0}
+						// 			className=" absolute top-0 -mr-4  right-0 h-screen z-[1] text-right  p-6  text-xl text-black font-bold dropdown-content bg-transparent  w-60 space-y-5 gap-5 -mt-4 pt-8"
+						// 		>
+						// 			<li >
+						// 				<Link to="/">
+						// 					Dashboard
+						// 				</Link>
+						// 			</li>
+						// 			<li>
+						// 				<Link
+						// 					onClick={() =>
+						// 						logOut()
+						// 					}
+						// 				>
+						// 					Logout
+						// 				</Link>
+						// 			</li>
+						// 		</ul>
+						// 	</div>
+						// </div>
 
-							<div className="navbar p-0 bg-base-100">
-
-								<div className="navbar-start">
-									<Link
-										to="/"
-										className="btn btn-ghost text-xl"
-									>
-										<img
-											className="h-14"
-											src={logo}
-											alt=""
-										/>
-									</Link>
-								</div>
-								<div className="navbar-center hidden lg:flex">
-									<ul className="menu menu-horizontal gap-4 px-1">
-										<NavUl></NavUl>
-									</ul>
-								</div>
-								{/* Login Logout toggle button logic */}
-								<div className="navbar-end mr-2">
-									{user?.email ? (
-										<div className="flex gap-3 items-center">
-											<div className="dropdown dropdown-end">
-												<div
-													tabIndex={0}
-													role="button"
-													className="btn btn-ghost btn-circle avatar"
-												>
-													<div id="my-drawer-2" className="w-10 rounded-full">
-														{
-                                                            user?.photoURL ? <img src={user.photoURL} alt="" /> : <img 
-															alt="Tailwind CSS Navbar component"
-															src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-														/>
-                                                        }
-													</div>
-												</div>
-												<ul style={{backdropFilter: "blur(30px)"}}
-													tabIndex={0}
-													className=" absolute top-0 -mr-4  right-0 h-screen z-[1] text-right  p-6  text-xl text-black font-bold dropdown-content bg-transparent  w-60 space-y-5 gap-5 -mt-4 pt-8"
-												>
-													<li >
-														<Link to="/">
-															Dashboard
-														</Link>
-													</li>
-													<li>
-														<Link
-															onClick={() =>
-																logOut()
-															}
-														>
-															Logout
-														</Link>
-													</li>
-												</ul>
-											</div>
-										</div>
-									) : (
-										<Link to="/login" className="btn">
-											Login
-										</Link>
-									)}
-								</div>
-							</div>
+						<div className="space-x-4">
+							<Link to="/dashboard/overView">
+                            <button className="btn hover:border-primaryColor hover:bg-primaryColor hover:text-white  btn-outline rounded-none text-primaryColor ">
+								Dashboard
+							</button>
+                        </Link>
+							<button  onClick={() => logOut()} className="btn hidden md:block hover:border-primaryColor hover:bg-primaryColor hover:text-white btn-outline rounded-none  text-primaryColor ">
+								Logout
+							</button>
 						</div>
-    );
+					) : (
+						<Link to="/login" className="btn">
+							Login
+						</Link>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navrouts;
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,16 +1,11 @@
 import { Link } from "react-router-dom";
 import money from "../../assets/Banner/money.png";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 const Banner = () => {
-<<<<<<< HEAD
+	const { user } = useContext(AuthContext);
 	return (
-		<div className="flex flex-col md:flex-row justify-center items-center rounded-br-[300px] bg-gradient-to-br from-[#23A455] to-[#34D399] mt-4 h-[600px]  bg-black   bg-cover bg-center">
-			
-
-
-
-
-      
-
+		<div className="flex flex-col md:flex-row justify-center items-center rounded-br-[300px] bg-gradient-to-br from-[#23A455] to-[#34D399]  h-[600px]  bg-black   bg-cover bg-center">
 			<div className="max-w-[500px] mt-10 p-4 space-y-7">
 				<h1 className="text-4xl lg:text-6xl  font-bold text-white ">
 					Get an easy <br /> ways to manage your finence{" "}
@@ -20,26 +15,19 @@ const Banner = () => {
 					account and finance platform – your key to smart, secure,
 					and simplified money management!
 				</p>
-				<Link to="/login">
-          <button className="btn btn-outline font-bold  text-white mt-4 ">
-            GEt STARTED
-=======
-  return (
-    <div className="bg-[url('https://i.ibb.co/M9Sp7Nx/businessmen-hands-white-table-with-documents-drafts.jpg')] h-[300px] lg:h-[600px] rounded bg-black bg-blend-overlay bg-opacity-60 bg-cover bg-center">
-      <div className="text-center pt-[70px] md:pt-[90px] w-full lg:p-[150px] ">
-        <h2 className="text-white lg:text-3xl  font-bold italic">
-          Transform your finances effortlessly with our dynamic account and
-          finance platform – your key to smart, secure, and simplified money
-          management!
-        </h2>
-      </div>
-      <div className="text-center mt-8 lg:-mt-[120px]">
-        <Link to="/login">
-          <button className="btn font-bold bg-emerald-400 text-white border-none  ">
-            Login Now
->>>>>>> 06788017c9bd1994c17b54c5ef40b7cf7b959ebc
-          </button>
-        </Link>
+				{user ? (
+					<Link to="/dashboard/overView">
+						<button className="btn btn-outline font-bold  text-white mt-4 ">
+							 Dashboard
+						</button>
+					</Link>
+				) : (
+					<Link to="/login">
+						<button className="btn btn-outline font-bold  text-white mt-4 ">
+							GEt STARTED
+						</button>
+					</Link>
+				)}
 			</div>
 			<div className="w-full lg:w-1/3">
 				<img className="w-full" src={money} alt="" />
