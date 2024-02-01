df067cf50b224f9a938e9c88b5cff0a7f744532d
mehedi505991 <mdmehediislammoksedul@gmail.com>
2024-02-01 21:42:40 +0600

    update accounts section

--- src/Dashboard/Accounts.jsx
+++ src/Dashboard/Accounts.jsx
@@ -29,45 +29,47 @@ const Accounts = () => {
 
   return (
     <div>
-      <div className="border-t-2 border-b-2 h-16 flex justify-between pt-4 pl-4 pr-4 mb-2 ">
-        <div>
-          <h1 className=" text-[#6C6C6C]"> Accounts</h1>
-        </div>
-
-        <div className="flex justify-end gap-10 ">
+      <div className="pt-20">
+        <div className="border-t-2 border-b-2 h-16 flex justify-between pt-4 pl-4 pr-4 mb-2 ">
           <div>
-            {/* <FaRegChartBar style={20} /> */}
+            <h1 className=" text-[#6C6C6C]"> Accounts</h1>
+          </div>
+
+          <div className="flex justify-end gap-10 ">
+            <div>
+              {/* <FaRegChartBar style={20} /> */}
+            </div>
           </div>
         </div>
-      </div>
-      <div className=" px-5 mb-2 grid grid-cols-3 ">
-        <div>
-          <CiBank className="text-2xl" />
+        <div className=" px-5 mb-2 grid grid-cols-3 ">
+          <div>
+            <CiBank className="text-2xl" />
+          </div>
+          <div></div>
+          {/* Total  */}
+          <div className="flex items-center justify-end gap-2">
+            <h2 className="  text-[#6C6C6C]">Total</h2>
+            <p className="text-[#181818] ">$ {total}</p>
+          </div>
         </div>
-        <div></div>
-        {/* Total  */}
-        <div className="flex items-center justify-end gap-2">
-          <h2 className="  text-[#6C6C6C]">Total</h2>
-          <p className="text-[#181818] ">$ {total}</p>
+        <div className="grid grid-cols-3 bg-[#F7F7FA] rounded h-10 border-t-2">
+          <p className="mt-2 ml-5 text-[#AEAEAF]">Account</p>
+          <p className="mt-2 flex justify-center text-[#AEAEAF]">Group</p>
+          <p className="mt-2 mr-5 flex justify-end text-[#AEAEAF]">Amount</p>
         </div>
-      </div>
-      <div className="grid grid-cols-3 bg-[#F7F7FA] rounded h-10 border-t-2">
-        <p className="mt-2 ml-5 text-[#AEAEAF]">Account</p>
-        <p className="mt-2 flex justify-center text-[#AEAEAF]">Group</p>
-        <p className="mt-2 mr-5 flex justify-end text-[#AEAEAF]">Amount</p>
-      </div>
-      <div className="grid grid-cols-1">
-        {Balance.map((Balances) => (
-          <AccountCard key={Balances._id} Balances={Balances}></AccountCard>
-        ))}
-      </div>
-      <div className="bg-[#F7F7FA]  h-36">
-        <div className="flex justify-end mr-5 pt-14">
-          <Link to="/dashboard/addBalance">
-            <div className="bg-[#3EEE2A]  w-6 h-5 pr-8 pl-4 pt-4 pb-8 rounded-full">
-              <AiOutlinePlus className="text-black font-bold " style={20} />
-            </div>
-          </Link>
+        <div className="grid grid-cols-1">
+          {Balance.map((Balances) => (
+            <AccountCard key={Balances._id} Balances={Balances}></AccountCard>
+          ))}
+        </div>
+        <div className="bg-[#F7F7FA]  h-36">
+          <div className="flex justify-end mr-5 pt-14">
+            <Link to="/dashboard/addBalance">
+              <div className="bg-[#3EEE2A]  w-6 h-5 pr-8 pl-4 pt-4 pb-8 rounded-full">
+                <AiOutlinePlus className="text-black font-bold " style={20} />
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
