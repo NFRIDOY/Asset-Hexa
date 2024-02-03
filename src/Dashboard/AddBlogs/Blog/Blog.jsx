435a86688e237d143a861f5d3b821c8f6bcb0e48
Radiat09 <rzsianboyridoy10@gmail.com>
2024-02-04 00:53:22 +0600

    modified the design of BlogCard of Blog page

--- src/Dashboard/AddBlogs/Blog/Blog.jsx
+++ src/Dashboard/AddBlogs/Blog/Blog.jsx
@@ -1,27 +1,28 @@
 import { useEffect, useState } from "react";
 import useAxios from "../../../hooks/useAxios";
 import BlogCard from "../BlogCard/BlogCard";
+import axios from "axios";
 import Loader from "../../../Route/loader";
 
 const Blog = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [Blogg, SetBlogs] = useState([]);
   const axiosPublic = useAxios();
   useEffect(() => {
     setLoading(true);
-    axiosPublic.get("/blogs").then((data) => {
-      SetBlogs(data?.data);
+    axios.get("http://localhost:5000/blogs").then((data) => {
+      SetBlogs(data.data);
       setLoading(false);
-      console.log(data.data);
+      // console.log(data.data);
     });
   }, [axiosPublic]);
-  // console.log(Blogg);
+  console.log(Blogg);
 
   if (loading) return <Loader />;
 
   return (
     <div className="mt-10 mb-40">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-5 max-w-7xl mx-auto">
         {Blogg?.map((Bloggs) => (
           <BlogCard key={Bloggs._id} Bloggs={Bloggs}></BlogCard>
         ))}
