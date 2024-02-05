8c40a7336857f6aaa3ddd85998d9ff1ad9957688
NF RIDOY <nfridoy@gmail.com>
2024-02-05 17:47:20 +0600

    besic stucture of invenstment

--- src/Dashboard/Investments/Investments.jsx
+++ src/Dashboard/Investments/Investments.jsx
@@ -1,7 +1,17 @@
+import InvestmentTable from "./investmentTable";
 
 
 export default function Investments() {
     return (
-        <div>Investments</div>
+        // <div style={{ border: "2px solid red" }} className="h-[calc(100vh-32px)] p-5">
+        <div className="h-screen p-5">
+            <h1 className="text-center text-4xl font-bold">Investments</h1>
+            <section className="flex">
+                <div className=" border-2 h-60 w-60"> Pi Chart </div>
+                <div className="table investment">
+                    <InvestmentTable />
+                </div>
+            </section>
+        </div>
     )
 }
