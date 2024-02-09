bbd6edde3669792820074cefc5d17c2d210f6e28
Radiat09 <rzsianboyridoy10@gmail.com>
2024-02-09 12:43:48 +0600

    added blog verification mark on blog details page and blogCard component

--- src/Pages/BlogDetails/BlogDetails.jsx
+++ src/Pages/BlogDetails/BlogDetails.jsx
@@ -9,6 +9,7 @@ import Swal from "sweetalert2";
 import useAxios from "../../hooks/useAxios";
 import toast from "react-hot-toast";
 import useBookmarked from "../../hooks/useBookmarked";
+import { MdVerified } from "react-icons/md";
 
 //http://localhost:5000\
 
@@ -33,6 +34,7 @@ const BlogDetails = () => {
     dislikes,
     comments,
     time,
+    isVerified,
   } = blog;
 
   useEffect(() => {
@@ -268,8 +270,13 @@ const BlogDetails = () => {
           </div>
         </div>
         <div>
-          <h1 className="text-3xl md:text-5xl font-bold mb-6 break-words">
-            {title || ""}
+          <h1 className="text-3xl md:text-5xl font-bold mb-6 break-words flex items-center lg:items-end">
+            {title || ""}{" "}
+            <span>
+              {isVerified && (
+                <MdVerified className="ml-2 text-4xl text-blue-500" />
+              )}
+            </span>
           </h1>
           <p className="text-lg font-normal">{description || ""}</p>
         </div>
