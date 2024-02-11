66d6b7959432e287a43340d44f1cacb142914cfa
Mustafiz82e <mustafiz8260@gamil.com>
2024-02-11 23:14:13 +0600

    admin dashboard is now device freindly

--- src/AdminDashboard/AdminOverview.jsx
+++ src/AdminDashboard/AdminOverview.jsx
@@ -32,28 +32,28 @@ const AdminOverview = () => {
 
 	const COLORS = ["#317DF0", "#F8A11B"];
 	return (
-		<div className="p-4 bg-base-300">
-			<div className="bg-white p-4 flex rounded-xl gap-3 overflow-x-auto min-h-40">
-				<div className="space-y-2 py-8 overflow-scroll scrollable-content  text-white rounded-xl bg-gradient-to-br from-[#449B38] to-[#34D399]  px-8 min-w-48 md:min-w-56 ">
+		<div className="p-4 lg:h-screen bg-base-300">
+			<div className="bg-white  p-4 flex lg:overflow-x-hidden overflow-x-scroll   rounded-xl gap-3  ">
+				<div className="space-y-2 min-w-60 lg:min-w-0  flex-1 py-8  overflow-scroll scrollable-content  text-white rounded-xl bg-gradient-to-br from-[#449B38] to-[#34D399]  px-8  ">
 					<h1 className="text-base font-medium">total Users</h1>
 					<p className="text-3xl md:text-5xl font-semibold">{adminState?.userCount}</p>
 				</div>
-				<div className="space-y-2 py-8 overflow-scroll scrollable-content  text-white rounded-xl bg-gradient-to-br from-[#F49328] to-[#E92A31]  px-8  min-w-48 md:min-w-56 ">
+				<div className="space-y-2 min-w-60 lg:min-w-0 flex-1  py-8  overflow-scroll scrollable-content  text-white rounded-xl bg-gradient-to-br from-[#F49328] to-[#E92A31]  px-8    ">
 					<h1 className="text-base font-medium">Total Transection</h1>
 					<p className="text-3xl md:text-5xl font-semibold">{adminState?.transectionsCount}</p>
 				</div>
-				<div className="space-y-2 py-8 overflow-scroll scrollable-content  text-white rounded-xl bg-gradient-to-br from-[#49a7e0] to-[#8fd6ff]  px-8  min-w-48 md:min-w-56">
+				<div className="space-y-2 min-w-60 lg:min-w-0 flex-1 py-8  overflow-scroll scrollable-content  text-white rounded-xl bg-gradient-to-br from-[#49a7e0] to-[#8fd6ff]  px-8  ">
 					<h1 className="text-base font-medium">Total Blogs</h1>
 					<p className="text-3xl md:text-5xl font-semibold">{adminState?.blogCount}</p>
 				</div>
-				<div className="space-y-2 overflow-scroll scrollable-content py-8 text-white rounded-xl bg-gradient-to-br from-[#FFE338] to-[#e94444]  px-8 min-w-48 md:min-w-56 ">
+				<div className="space-y-2 min-w-60 lg:min-w-0 flex-1 overflow-scroll scrollable-content py-8  text-white rounded-xl bg-gradient-to-br from-[#FFE338] to-[#e94444]  px-8  ">
 					<h1 className="text-base font-medium">
 						{" "}
 						Business posting{" "}
 					</h1>
 					<p className="text-3xl md:text-5xl font-semibold">{adminState?.businessCount}</p>
 				</div>
-				<div className="space-y-2 overflow-scroll scrollable-content py-8 text-white rounded-xl bg-gradient-to-br from-purple-700 to-purple-400  px-8 min-w-48 md:min-w-56 ">
+				<div className="space-y-2 min-w-60 lg:min-w-0 flex-1 overflow-scroll scrollable-content py-8  text-white rounded-xl bg-gradient-to-br from-purple-700 to-purple-400  px-8  ">
 					<h1 className="text-base font-medium">
 						{" "}
 						Newslater subscitption
@@ -62,8 +62,8 @@ const AdminOverview = () => {
 				</div>
 			</div>
 
-			<div className="flex flex-col md:flex-row gap-5 md:mt-5 my-5   ">
-				<div className="bg-white  md:p-10 mx-auto">
+			<div className="flex flex-col lg:flex-row justify gap-5   md:mt-5 my-5   ">
+				<div className="bg-white w-full flex justify-center items-center h-0 flex-1 lg:h-[calc(100vh-250px)] mx-auto">
 					<PieChart width={350} height={350}>
 						<Pie
 							dataKey="value"
@@ -88,7 +88,7 @@ const AdminOverview = () => {
 					</PieChart>{" "}
 				</div>
 
-				<div className="bg-white w-full p-6 px-10 flex flex-col md:flex-row gap-5 items-center justify-between">
+				<div className="bg-white w-full lg:h-[calc(100vh-250px)]  p-6 px-10 flex flex-col md:flex-row gap-5 items-center justify-between">
 					<Link to="/AdminDashboard/BusinessVerification"  className="w-72   md:w-48 shadow-custom flex flex-col justify-center items-center h-72 md:h-48 md:-mt-16 ">
 						<div className="relative">
 							<IoMdBusiness size={72} className="!z-[-999]" />
