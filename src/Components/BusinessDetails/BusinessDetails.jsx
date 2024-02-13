4e25c179d3832a82fb8175cf0b66281b82311240
NF RIDOY <nfridoy@gmail.com>
2024-02-13 19:41:18 +0600

    [feat](business card and details): total amount

--- src/Components/BusinessDetails/BusinessDetails.jsx
+++ src/Components/BusinessDetails/BusinessDetails.jsx
@@ -7,15 +7,17 @@ import useAdmin from "../../hooks/useAdmin";
 import InvestmentRow from './../../Dashboard/Investments/InvestmentRow';
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
+import { useState } from "react";
 
 
 const BusinessDetails = () => {
     const { id } = useParams();
-    const { business } = useBusiness(id);
+    const { business, refetch } = useBusiness(id);
     const axiosPublic = useAxios()
     const { user } = useAuth();
+    const [totalInvestments, setTotalInvestment] = useState(business?.totalInvestment)
 
-    // console.log(business);
+    console.log(business);
 
     const handleVerification = () => {
         axiosPublic.put(`/business/${id}`)
@@ -38,20 +40,29 @@ const BusinessDetails = () => {
         const form = e.target;
         const invest = parseFloat(form.invest.value);
         console.log(invest);
+
+        setTotalInvestment(totalInvestments+invest);
+        // const totalAmount = business.totalInvestment.reduce(
+        //     (total, item) => total + parseInt(item.amount),
+        //     0
+        // );
+
         const InvestmentObj = {
             investor: user?.email,
             invest: invest
         }
 
         axiosPublic.put(`/businessInvest/${id}`, InvestmentObj)
-        // axios.put(`http://localhost:5000/businessInvest/${id}`, InvestmentObj)
+            // axios.put(`http://localhost:5000/businessInvest/${id}`, InvestmentObj)
             .then(res => {
                 console.log(res.data);
+                refetch();
                 if (res?.data.result.modifiedCount >= 1) {
                     toast.success("Invested on this Business ");
+                    refetch();
                     if (res?.data.addToInvestments.modifiedCount >= 1) {
                         toast.success("Added to Your Investments");
-
+                        refetch();
                     }
 
                 }
@@ -68,9 +79,9 @@ const BusinessDetails = () => {
         <div className="max-w-7xl mx-auto px-2">
             <div className=" ">
                 <div className="">
-                    <img className="md:w-[1200px]  rounded-xl h-96 md:h-96 lg:h-[450px]" src={business?.BannerImage} alt="" />
+                    <img className="md:w-full  rounded-xl h-96 md:h-96 lg:h-[450px]" src={business?.BannerImage} alt="" />
                 </div>
-                <div className="md:flex md:justify-between  items-center gap-3">
+                <div className="md:flex md:justify-between  items-center gap-3 ">
                     <div className="flex justify-center items-center">
                         <div>
                             <h2 className="text-3xl">
@@ -84,39 +95,42 @@ const BusinessDetails = () => {
                                 alt=""
                             />
                             <div>
-                                <p className="font-bold text-xl md:text-2xl">{business?.userName}</p>
+                                <p className="font-bold text-xl md:text-2xl">{business?.userName} ({business?.Designation})</p>
                                 <p className="text-black">{business?.userEmail}</p>
                                 <p>{business?.time}</p>
                             </div>
                         </div>
                     </div>
-                    <form onSubmit={handleInvest} className="flex lg:pr-32 mt-5 ml-10 ">
-                        <input className="shadow-xl outline-none input input-bordered input-primary" type="number " name="invest" id="invest" />
-                        <button className="btn bg-[#4FD28C]  " type="submit">Investment</button>
-
-                    </form>
-
-
+                    <div>
+                        <p className="font-bold text-3xl ">
+                            Total Investments BDT :
+                            <span className="text-[#23A455]"> {totalInvestments}
+                            </span>
+                        </p>
+                    </div>
                 </div>
 
             </div>
 
             <div className="mt-5">
                 <div className=" md:flex justify-between items-center">
-                    <div className=" flex items-center gap-3">
-                        <img
-                            className="w-14 h-14 rounded-full"
-                            src={business?.BrandImage}
-                            alt=""
-                        />
-                        <div>
-                            <p className="font-bold">{business?.CompanyName}</p>
-
+                    <div className="flex justify-between items-center w-96">
+                        <div className=" flex items-center gap-3">
+                            <img
+                                className="w-14 h-14 rounded-full"
+                                src={business?.BrandImage}
+                                alt=""
+                            />
+                            <div>
+                                <p className="font-bold">{business?.CompanyName}</p>
+                            </div>
                         </div>
+
                     </div>
                     <div className="md:mr-32">
+                        Investment Range:
                         <p className="card-title text-success">
-                            Range: {business?.Minimum} BDT -  {business?.Maximum} BDT
+                            {business?.Minimum} BDT -  {business?.Maximum} BDT
                         </p>
                         <p>
                             Profit {business?.Profit} % Per Mounth
@@ -124,17 +138,22 @@ const BusinessDetails = () => {
                     </div>
 
                 </div>
-                <div className="mt-10 pr-3 pl-3 ">
+                <div className="mt-10 pr-3 pl-3 text-justify">
 
                     <p className="">
                         {business?.CompanyDescription}
 
                     </p>
 
-                    {isAdmin && <button onClick={handleVerification} className="btn mt-4 bg-gradient-to-r from-[#23A455] via-[#2ecc71] to-[#34D399] hover:border-none  border-none hover:bg-primaryColor  text-white  btn-outline  mt-2 rounded-md">
+                    {isAdmin && <button onClick={handleVerification} className="btn mt-4 bg-gradient-to-r from-[#23A455] via-[#2ecc71] to-[#34D399] hover:border-none  border-none hover:bg-primaryColor  text-white  btn-outline rounded-md">
                         varify this business
                     </button>}
                 </div>
+                <form onSubmit={handleInvest} className="flex justify-end lg:pr-32 mt-5 ml-10 ">
+                    <input className="shadow-xl outline-none input input-bordered input-primary" type="number " name="invest" id="invest" />
+                    <button className="btn bg-[#4FD28C]  " type="submit">Investment</button>
+
+                </form>
             </div>
 
         </div>
