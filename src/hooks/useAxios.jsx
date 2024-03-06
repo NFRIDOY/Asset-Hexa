dcd06c3c33d7d97fc5e694e50ff55723a55d8773
mehedi505991 <mdmehediislammoksedul@gmail.com>
2024-03-07 01:20:59 +0600

    update business code

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  // baseURL: "https://asset-hexa-server.vercel.app",
-  baseURL: "http://localhost:5000",
-<<<<<<< HEAD
-
-=======
->>>>>>> bec97ce0f81298becfb38f2c0f5651b7adeeac4c
+  baseURL: "https://asset-hexa-server.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 const useAxios = () => {
